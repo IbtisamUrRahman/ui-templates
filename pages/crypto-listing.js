67303631a4010b2c9d5e8dd66dd88e4384d18734
zaryabzaib <zaryabzaib80@gmail.com>
2021-05-11 11:17:18 +0500

    created component crypto-listing

---
+++ pages/crypto-listing.js
@@ -0,0 +1,9 @@
+const CryptoListing = () => {
+    return ( 
+        <div className="text-lg bg-gray-900 text-white">
+            Hey there!
+        </div>
+     );
+}
+ 
+export default CryptoListing;
