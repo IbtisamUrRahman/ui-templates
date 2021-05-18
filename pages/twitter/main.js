a681246a5da3e486d22b7a7b6e71478e7b118006
zaryabzaib <zaryabzaib80@gmail.com>
2021-05-18 11:08:04 +0500

    was working on crypto table but facing some errors now started working on twitter clone

---
+++ pages/twitter/main.js
@@ -0,0 +1,13 @@
+const twitterMain = () => {
+    return ( 
+        <>
+        <div className="flex items-center">
+            <div className="w-1/4">aaaaaaaaaaaaaaaaaaa</div>
+            <div className="w-1/2">bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb</div>
+            <div className="w-1/2">ccccccccccccccccccc</div>
+        </div>
+        </>
+     );
+}
+ 
+export default twitterMain;
