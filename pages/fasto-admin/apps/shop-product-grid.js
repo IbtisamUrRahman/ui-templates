3cb94856e2625c0b0f296ec67019158fc5c957c3
zaryabzaib <zaryabzaib80@gmail.com>
2021-05-27 14:14:16 +0500

    created shop/product-grid and shop/product-list pages with all responsiveness.

---
+++ pages/fasto-admin/apps/shop-product-grid.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import DashboardLayout from '../../../components/dashboard-layout/dashboard-layout';
+import { RiStarSFill, RiStarSLine } from "react-icons/ri";
+
+const ProductGrid = () => {
+    return ( 
+        <>
+        <DashboardLayout>
+        <div className="m-4 py-4 px-6 shadow-sm rounded-lg">
+                <div className="flex justify-between items-center">
+                    <div>
+                        <h2 className="font-semibold text-2xl tracking-wider">Hi, Welcome back</h2>
+                        <p className="text-gray-500 text-lg tracking-wide">Your Mock Web</p>
+                    </div>
+                    <div className="flex">
+                        <span className="text-lg mr-3">Layout</span>
+                        <span className="text-lg text-gray-500 mr-3">/</span>
+                        <span className="text-lg text-gray-500">Blank</span>
+                    </div>
+                </div>
+            </div>
+        <div className="m-4 py-4 px-6 shadow-sm rounded-lg">
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+                <div className="p-3 rounded-lg shadow-lg">
+                    <img className="h-96 w-64 object-cover mx-auto" src="https://images.unsplash.com/photo-1521577352947-9bb58764b69a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGZlbWFsZSUyMG1vZGVsc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"></img>
+                    <span className="block text-lg xl:text-2xl text-center mt-2">Benorum et Molorum</span>
+                    <div className="flex justify-center mt-2">
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSLine/>
+                    </div>
+                    <span className="block text-xl font-semibold text-center mt-2">$ 761.00</span>
+                </div>
+                <div className="p-3 rounded-lg shadow-lg">
+                    <img className="h-96 w-64 object-cover mx-auto" src="https://images.unsplash.com/photo-1616244094867-6efbbd4d1ddd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHBvbGthJTIwZG90cyUyMGRyZXNzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"></img>
+                    <span className="block text-lg xl:text-2xl text-center mt-2">Polka Dots</span>
+                    <div className="flex justify-center mt-2">
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSLine/>
+                    </div>
+                    <span className="block text-xl font-semibold text-center mt-2">$ 139.00</span>
+                </div>
+                <div className="p-3 rounded-lg shadow-lg">
+                    <img className="h-96 w-64 object-cover mx-auto" src="https://images.unsplash.com/photo-1576558345433-58e777a5e423?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGxhaW4lMjBibGFjayUyMGRyZXNzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"></img>
+                    <span className="block text-lg xl:text-2xl text-center mt-2">Black Hoodie</span>
+                    <div className="flex justify-center mt-2">
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                    </div>
+                    <span className="block text-xl font-semibold text-center mt-2">$ 40.00</span>
+                </div>
+                <div className="p-3 rounded-lg shadow-lg">
+                    <img className="h-96 w-64 object-cover mx-auto" src="https://images.unsplash.com/photo-1596703343725-7ca01bda9a45?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Ymx1ZSUyMGRyZXNzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"></img>
+                    <span className="block text-lg xl:text-2xl text-center mt-2">Back Bug</span>
+                    <div className="flex justify-center mt-2">
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSLine/>
+                    </div>
+                    <span className="block text-xl font-semibold text-center mt-2">$ 500.00</span>
+                </div>
+                <div className="p-3 rounded-lg shadow-lg">
+                    <img className="h-96 w-64 object-cover mx-auto" src="https://images.unsplash.com/photo-1602423763918-6ae68bca77c8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTU1fHxibHVlJTIwZHJlc3N8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"></img>
+                    <span className="block text-lg xl:text-2xl text-center mt-2">Fox Sake</span>
+                    <div className="flex justify-center mt-2">
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSLine/>
+                        <RiStarSLine/>
+                    </div>
+                    <span className="block text-xl font-semibold text-center mt-2">$ 254.00</span>
+                </div>
+                <div className="p-3 rounded-lg shadow-lg">
+                    <img className="h-96 w-64 object-cover mx-auto" src="https://images.unsplash.com/photo-1602414450735-6fb78e251a17?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTczfHxibHVlJTIwZHJlc3N8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"></img>
+                    <span className="block text-lg xl:text-2xl text-center mt-2">Chinese Traditional</span>
+                    <div className="flex justify-center mt-2">
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSLine/>
+                        <RiStarSLine/>
+                    </div>
+                    <span className="block text-xl font-semibold text-center mt-2">$ 630.50</span>
+                </div>
+            </div>
+        </div>
+        </DashboardLayout>
+        </>
+     );
+}
+ 
+export default ProductGrid;
---
+++ pages/fasto-admin/apps/shop-product-list.js
@@ -0,0 +1,174 @@
+import React from 'react';
+import DashboardLayout from '../../../components/dashboard-layout/dashboard-layout';
+import { RiStarSFill, RiStarSLine } from "react-icons/ri";
+import { AiFillCheckCircle } from "react-icons/ai";
+
+const ProductList = () => {
+    return ( 
+        <>
+        <DashboardLayout>
+            <div className="m-4 py-4 px-6 shadow-sm rounded-lg">
+                <div className="flex justify-between items-center">
+                    <div>
+                        <h2 className="font-semibold text-2xl tracking-wider">Hi, Welcome back</h2>
+                        <p className="text-gray-500 text-lg tracking-wide">Your Mock Web</p>
+                    </div>
+                    <div className="flex">
+                        <span className="text-lg mr-3">Layout</span>
+                        <span className="text-lg text-gray-500 mr-3">/</span>
+                        <span className="text-lg text-gray-500">Blank</span>
+                    </div>
+                </div>
+            </div>
+
+            <div className="xl:grid xl:grid-cols-2 xl:gap-4">
+            <div className="mx-4 my-1 md:mx-4 md:my-2 xl:mx-3 py-4 px-6 xl:p-0">
+                <div className="px-6 py-8 md:py-4 rounded-lg shadow-md md:flex md:justify-between xl:block">
+                    <img className="h-auto w-full md:object-cover md:h-80 md:mr-6 xl:h-96" src="https://images.unsplash.com/photo-1521577352947-9bb58764b69a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGZlbWFsZSUyMG1vZGVsc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"></img>
+                    <div className="my-3 md:my-1">
+                        <span className="block text-lg font-semibold my-4 md:my-0 md:-mt-1 md:mb-2 xl:mt-2">Solid Women Monetero Dress</span>
+                        <div className="flex">
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSLine/>
+                        </div>
+                        <div className="flex justify-between">
+                            <div className="flex">
+                                <span className="text-gray-500 mr-2">(34 Reviews)</span>
+                                <span className="text-gray-500 mr-2">/</span>
+                                <span className="text-gray-500">Write a review ?</span>
+                            </div>
+                            <div>
+                                <span className="text-xl font-semibold">$ 761.00</span>
+                            </div>
+                        </div>
+                        <div className="flex">
+                            <span className="text-gray-500 mr-2">Avaibility:</span>
+                            <div className="flex items-center">
+                                <span className="text-gray-500 mr-2">in stocks</span>
+                                <AiFillCheckCircle/>
+                            </div>
+                        </div>
+                        <span className="text-gray-500 block mt-2">Product code: 064552</span>
+                        <span className="text-gray-500 block mt-2">Brand: Lee</span>
+                        <p className="text-gray-500 leading-relaxed mt-2">In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="mx-4 my-1 md:mx-4 md:my-2 xl:mx-3 py-4 px-6 xl:p-0">
+                <div className="px-6 py-8 md:py-4 rounded-lg shadow-md md:flex md:justify-between xl:block">
+                    <img className="h-auto w-full md:object-cover md:h-80 md:mr-6 xl:h-96" src="https://images.unsplash.com/photo-1596703343725-7ca01bda9a45?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Ymx1ZSUyMGRyZXNzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"></img>
+                    <div className="my-3 md:my-1">
+                        <span className="block text-lg font-semibold my-4 md:my-0 md:-mt-1 md:mb-2 xl:mt-2">Back Bug</span>
+                        <div className="flex">
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSLine/>
+                        </div>
+                        <div className="flex justify-between">
+                            <div className="flex">
+                                <span className="text-gray-500 mr-2">(34 Reviews)</span>
+                                <span className="text-gray-500 mr-2">/</span>
+                                <span className="text-gray-500 block">Write a review ?</span>
+                            </div>
+                            <div>
+                                <span className="text-xl xl:font font-semibold">$ 500.00</span>
+                            </div>
+                        </div>
+                        <div className="flex">
+                            <span className="text-gray-500 mr-2">Avaibility:</span>
+                            <div className="flex items-center">
+                                <span className="text-gray-500 mr-2">in stocks</span>
+                                <AiFillCheckCircle/>
+                            </div>
+                        </div>
+                        <span className="text-gray-500 block mt-2">Product code: 064552</span>
+                        <span className="text-gray-500 block mt-2">Brand: Fox Sake</span>
+                        <p className="text-gray-500 leading-relaxed mt-2">In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="mx-4 my-1 md:mx-4 md:my-2 xl:mx-3 py-4 px-6 xl:p-0">
+                <div className="px-6 py-8 md:py-4 rounded-lg shadow-md md:flex md:justify-between xl:block">
+                    <img className="h-auto w-full md:object-cover md:h-80 md:mr-6 xl:h-96" src="https://images.unsplash.com/photo-1616244094867-6efbbd4d1ddd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHBvbGthJTIwZG90cyUyMGRyZXNzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"></img>
+                    <div className="my-3 md:my-1">
+                        <span className="block text-lg font-semibold my-4 md:my-0 md:-mt-1 md:mb-2 xl:mt-2">Polka Dots</span>
+                        <div className="flex">
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSLine/>
+                        </div>
+                        <div className="flex justify-between">
+                            <div className="flex">
+                                <span className="text-gray-500 mr-2">(34 Reviews)</span>
+                                <span className="text-gray-500 mr-2">/</span>
+                                <span className="text-gray-500 block">Write a review ?</span>
+                            </div>
+                            <div>
+                                <span className="text-xl xl:font font-semibold">$ 300.00</span>
+                            </div>
+                        </div>
+                        <div className="flex">
+                            <span className="text-gray-500 mr-2">Avaibility:</span>
+                            <div className="flex items-center">
+                                <span className="text-gray-500 mr-2">in stocks</span>
+                                <AiFillCheckCircle/>
+                            </div>
+                        </div>
+                        <span className="text-gray-500 block mt-2">Product code: 064552</span>
+                        <span className="text-gray-500 block mt-2">Brand: Fox Sake</span>
+                        <p className="text-gray-500 leading-relaxed mt-2">In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="mx-4 my-1 md:mx-4 md:my-2 xl:mx-3 py-4 px-6 xl:p-0">
+                <div className="px-6 py-8 md:py-4 rounded-lg shadow-md md:flex md:justify-between xl:block">
+                    <img className="h-auto w-full md:object-cover md:h-80 md:mr-6 xl:h-96" src="https://images.unsplash.com/photo-1576558345433-58e777a5e423?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGxhaW4lMjBibGFjayUyMGRyZXNzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"></img>
+                    <div className="my-3 md:my-1">
+                        <span className="block text-lg font-semibold my-4 md:my-0 md:-mt-1 md:mb-2 xl:mt-2">Black Hoodie</span>
+                        <div className="flex">
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSFill/>
+                        <RiStarSLine/>
+                        </div>
+                        <div className="flex justify-between">
+                            <div className="flex">
+                                <span className="text-gray-500 mr-2">(34 Reviews)</span>
+                                <span className="text-gray-500 mr-2">/</span>
+                                <span className="text-gray-500 block">Write a review ?</span>
+                            </div>
+                            <div>
+                                <span className="text-xl xl:font font-semibold">$ 139.00</span>
+                            </div>
+                        </div>
+                        <div className="flex">
+                            <span className="text-gray-500 mr-2">Avaibility:</span>
+                            <div className="flex items-center">
+                                <span className="text-gray-500 mr-2">in stocks</span>
+                                <AiFillCheckCircle/>
+                            </div>
+                        </div>
+                        <span className="text-gray-500 block mt-2">Product code: 064552</span>
+                        <span className="text-gray-500 block mt-2">Brand: Monark</span>
+                        <p className="text-gray-500 leading-relaxed mt-2">In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.</p>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </DashboardLayout>
+        </>
+     );
+}
+ 
+export default ProductList;
